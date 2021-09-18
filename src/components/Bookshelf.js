b4b1a3378c323fa9fd2f2d14ba65391a384fee1c
Andrew <wanger@v1040-wn-rt-c-51-43.campus-dynamic.uwaterloo.ca>
2021-09-18 18:19:59 -0400

    setup react with chrome

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Bookshelf extends React.Component {
+  render() {
+    return <h2>Hi, I am a Shelf!</h2>;
+  }
+}
+
+export default Bookshelf;
